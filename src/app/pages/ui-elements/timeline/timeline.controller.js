9bdcec5aec663cfb2a23e5ebea758a312fa60763
Erhan Kılıç <erhan@erhankilic.org>
2018-07-26 21:02:40 +0300

    Ui Elements / timeline page has been created.

---
+++ src/app/pages/ui-elements/timeline/timeline.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('elementsTimeline', {
+            url: '/ui-elements/timeline',
+            templateUrl: 'app/pages/ui-elements/timeline/timeline.html',
+            controller: ControllerFn,
+            controllerAs: 'vmTimeline'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+    }
+})();
