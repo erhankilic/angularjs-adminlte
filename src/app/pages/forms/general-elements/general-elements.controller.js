28f6411d05c8a22f205f663632aee0a8c726a6ad
Erhan Kılıç <erhan@erhankilic.org>
2018-07-28 15:02:17 +0300

    Forms / General Elements page created.

---
+++ src/app/pages/forms/general-elements/general-elements.controller.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('formsGeneralElements', {
+            url: '/',
+            templateUrl: 'app/pages/forms/general-elements/general-elements.html',
+            controller: ControllerFn,
+            controllerAs: 'vmGeneralElements'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+
+        $(document).ready(function () {
+        });
+    }
+})();
