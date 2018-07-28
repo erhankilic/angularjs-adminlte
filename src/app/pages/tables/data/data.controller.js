afb315aa66c5bf6d71c2cb3edeed04cf2e88f465
Erhan Kılıç <erhan@erhankilic.org>
2018-07-28 21:15:42 +0300

    Tables pages created.

---
+++ src/app/pages/tables/data/data.controller.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('tablesData', {
+            url: '/tables/data',
+            templateUrl: 'app/pages/tables/data/data.html',
+            controller: ControllerFn,
+            controllerAs: 'vmData'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+
+        $(document).ready(function () {
+            $('#example1').DataTable();
+            $('#example2').DataTable({
+                'paging'      : true,
+                'lengthChange': false,
+                'searching'   : false,
+                'ordering'    : true,
+                'info'        : true,
+                'autoWidth'   : false
+            })
+        });
+    }
+})();
---
+++ src/app/pages/tables/simple/simple.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('tablesSimple', {
+            url: '/tables/simple',
+            templateUrl: 'app/pages/tables/simple/simple.html',
+            controller: ControllerFn,
+            controllerAs: 'vmSimple'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+    }
+})();
