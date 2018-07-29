14a2649b51b7a0ccf2de5c188e29119225cef499
Erhan Kılıç <erhan@erhankilic.org>
2018-07-29 09:54:22 +0300

    Examples / 404 page has been created.

---
+++ src/app/pages/examples/404/404.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('examples404', {
+            url: '/examples/404',
+            templateUrl: 'app/pages/examples/404/404.html',
+            controller: ControllerFn,
+            controllerAs: 'vm404'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+    }
+})();
