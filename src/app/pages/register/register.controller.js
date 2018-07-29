3596837d87eac83d8335d42d17e251691d25413a
Erhan Kılıç <erhan@erhankilic.org>
2018-07-29 16:52:04 +0300

    Register page has been created.

---
+++ src/app/pages/register/register.controller.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('register', {
+            url: '/register',
+            views: {
+                'main@': {
+                    templateUrl: 'app/pages/register/register.html',
+                    controller: ControllerFn,
+                    controllerAs: 'vmRegister'
+                }
+            },
+            bodyClass: 'register-page'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+
+        $(document).ready(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' /* optional */
+            });
+        });
+    }
+})();
