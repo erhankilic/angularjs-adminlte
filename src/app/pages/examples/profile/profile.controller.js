fb7e54c68e09cea0af7784db1fa8d7564900da27
Erhan Kılıç <erhan@erhankilic.org>
2018-07-29 09:47:06 +0300

    Examples / Profile page has been created.

---
+++ src/app/pages/examples/profile/profile.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').config(configFn);
+
+    /**
+     * @ngInject
+     * @type {string[]}
+     */
+    configFn.$inject = ['$stateProvider'];
+
+    /**
+     * Config
+     *
+     * @param $stateProvider
+     */
+    function configFn($stateProvider) {
+        $stateProvider.state('examplesProfile', {
+            url: '/examples/profile',
+            templateUrl: 'app/pages/examples/profile/profile.html',
+            controller: ControllerFn,
+            controllerAs: 'vmProfile'
+        });
+    }
+
+    /**
+     * Controller Function
+     *
+     * @constructor
+     */
+    function ControllerFn() {
+        var vm = this;
+    }
+})();
