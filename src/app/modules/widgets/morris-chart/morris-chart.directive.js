0a56b7048a0007106bb9dcaf352068924ed24678
Erhan Kılıç <erhan@erhankilic.org>
2018-08-08 23:37:37 +0300

    Morris chart widget directive completed.

---
+++ src/app/modules/widgets/morris-chart/morris-chart.directive.js
@@ -0,0 +1,54 @@
+(function () {
+    'use strict';
+
+    angular.module('adminlte').directive('morrisChart', DirectiveFn);
+
+    /**
+     * Directive
+     * @returns {{restrict: string, $scope: {type: string, data: string}, bindToController: boolean, link: linkFn}}
+     * @constructor
+     */
+    function DirectiveFn() {
+
+        return {
+            restrict: "EA",
+            scope: {
+                type: "@",
+                redraw: "@",
+                data: "="
+            },
+            link: linkFn
+        };
+
+    }
+
+    /**
+     * Link Function
+     * @param $scope
+     * @param $element
+     * @param $attrs
+     */
+    function linkFn($scope, $element, $attrs)  {
+        var chart;
+
+        $(document).ready(function () {
+            if ($scope.type == "line") {
+                chart = new Morris.Line($scope.data);
+            } else if ($scope.type == "donut") {
+                chart = new Morris.Donut($scope.data);
+            } else if ($scope.type == "bar") {
+                chart = new Morris.Bar($scope.data);
+            } else {
+                chart = new Morris.Area($scope.data);
+            }
+        });
+
+        $scope.$watch("redraw", function (newVal) {
+            if ($scope.redraw) {
+                window.setTimeout(function () {
+                    chart.redraw();
+                }, 250);
+            }
+        })
+    }
+})();
